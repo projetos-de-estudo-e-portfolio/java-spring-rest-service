645e29cc2d6b1faeef17d3fbf76c6c6f2130b67a
Luana da Rosa <lrosa.silva@outlook.com>
2021-11-10 15:46:41 -0300

    initial project

---
+++ src/main/java/com/example/servicesspringjava/ServicesSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicesspringjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesSpringJavaApplication.class, args);
+	}
+
+}
